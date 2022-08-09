9f280495c6d0b7d69ad8a14e14d6d59f058bd568
hoangbh1104126 <110478946+hoangbh1104126@users.noreply.github.com>
2022-08-09 23:56:44 +0700

    Add files via upload

--- src/app/testform/testform.component.ts
+++ src/app/testform/testform.component.ts
@@ -1,1 +1,40 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
+@Component({
+  selector: 'app-testform',
+  templateUrl: './testform.component.html',
+  styleUrls: ['./testform.component.scss']
+})
+export class TestformComponent implements OnInit {
+  @Input() name !: string;
+  @Input() password : string = "password";
+  @Output() newNoteEvent = new EventEmitter<string>();
+
+  addNewNote(value : string): void{
+    this.newNoteEvent.emit(value);
+  }
+
+  count = -2;
+
+  constructor() {
+    alert("Component create!");
+  }
+
+  ngDoCheck() {
+    this.count++;
+    console.log("Changed!");
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+    alert("Component destroy ...");
+    alert("Bye!");
+  }
+
+  onSubmit(){
+    alert(this.name + "'s new password is: " + this.password);
+  }
+
+}
